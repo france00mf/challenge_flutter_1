f78c6399a7293464b61fabaec5632377c48f1dda
FranciscoJose00MF <franciscodomingosjose06798@gmail.com>
2020-07-08 22:07:29 +0100

    build: division layout home (Expanded)

--- lib/src/ui/home/home.dart
+++ lib/src/ui/home/home.dart
@@ -5,11 +5,31 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>{
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 50,
+            child: Container(
+                //color: Colors.lightBlue[400],
+                ),
+          ),
+          Expanded(
+            flex: 50,
+            child: Container(
+              color: Colors.blueGrey,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
