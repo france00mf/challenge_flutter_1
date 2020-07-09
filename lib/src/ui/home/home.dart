c6e016ce2c8b3aca897f60d1e934fe4434b0c12a
FranciscoJose00MF <franciscodomingosjose06798@gmail.com>
2020-07-09 14:04:45 +0100

    build: put text and icon on button continue

--- lib/src/ui/home/home.dart
+++ lib/src/ui/home/home.dart
@@ -40,11 +40,55 @@ class _HomeState extends State<Home> {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                      margin: EdgeInsets.only(right: 40, left: 40),
+                      child: Flex(
+                        direction: Axis.vertical,
+                        children: <Widget>[
+                          Expanded(
+                              flex: 30,
+                              child: Container(
+                                  //color: Colors.amber,
+                                  )),
+                          Expanded(
+                              flex: 70,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(50))),
+                              ))
+                        ],
+                      )),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text("Find a companion"),
+                          Icon(Icons.arrow_forward)
+                        ],
+                      ),
+                      margin: EdgeInsets.only(bottom: 30),
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: 80,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(28),
+                              bottomLeft: Radius.circular(28))),
+                    ),
+                  ),
+                ],
+              ),
               height: 415,
               margin: EdgeInsets.only(bottom: 40),
               decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.5),
-              ),
+                  //color: Colors.black.withOpacity(0.5),
+                  ),
             ),
           )
         ],
@@ -53,5 +97,9 @@ class _HomeState extends State<Home> {
   }
 }
 /*
-
+Container(
+                      width: 100,
+                      height: 40,
+                      color: Colors.pink,
+                    ),
 */
